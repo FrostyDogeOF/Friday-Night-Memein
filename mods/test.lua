c3d74219eb72defc8a56f843a7d3f7ea52956192
FrostyDogeOF <adrianstephanblaylock@outlook.com>
2025-02-13 09:00:45 -0600

    Initial commit

---
+++ mods/test.lua
@@ -0,0 +1,17 @@
+function onUpdate()
+makeLuaText('deaths12', lowQuality , 0, 0, 680)
+	setTextSize('deaths12', 13)
+	addLuaText('deaths12', true)
+
+makeLuaText('deaths1234', ghostTapping , 0, 0, 600)
+	setTextSize('deaths1234', 13)
+	addLuaText('deaths1234', true)
+
+makeLuaText('deaths2', hideHud, 0, 0, 580)
+	setTextSize('deaths2', 13)
+	addLuaText('deaths2', true)
+
+makeLuaText('deaths31', songName, 0, 0, 540)
+	setTextSize('deaths31', 13)
+	addLuaText('deaths31', true)
+	end
